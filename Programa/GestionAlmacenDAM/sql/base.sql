2d019480f6402e79bfc211c4a32145865e1284d9
Daniel Carmona <daniii10cr@gmail.com>
2022-05-09 20:59:05 +0200

    Cambio Proyecto a Maven y conexión con bbdd

---
+++ Programa/GestionAlmacenDAM/sql/base.sql
@@ -0,0 +1,80 @@
+CREATE TABLE IF NOT EXISTS "Article" (
+                                            "PIC" TEXT  NOT NULL,
+                                            "article" TEXT NOT NULL,
+                                            "description" TEXT NOT NULL,
+                                            "location" TEXT NOT NULL,
+                                            "price" REAL NOT NULL,
+                                            "stock" INTEGER NOT NULL,
+                                            "isActive" BOOLEAN NOT NULL,
+                                            PRIMARY KEY("PIC"));
+
+CREATE TABLE IF NOT EXISTS "Customer" (
+                                         "CIC" TEXT  NOT NULL,
+                                         "name" TEXT NOT NULL,
+                                         "surname" TEXT NOT NULL,
+                                         "cif" TEXT NOT NULL,
+                                         "direction" TEXT NOT NULL,
+                                         "telephoneNumber" String NOT NULL,
+                                         "email" TEXT NOT NULL,
+                                         "photo" TEXT NOT NULL,
+                                         "createdAt" TEXT NOT NULL,
+                                         PRIMARY KEY("CIC"));
+
+CREATE TABLE IF NOT EXISTS "Employee" (
+                                         "EIC" TEXT  NOT NULL,
+                                         "name" TEXT NOT NULL,
+                                         "surname" TEXT NOT NULL,
+                                         "nif" TEXT NOT NULL,
+                                         "email" TEXT NOT NULL,
+                                         "photo" TEXT NOT NULL,
+					                    "idUser" INTERGER NOT NULL,
+                                         "isManager" BOOLEAN NOT NULL,
+                                         "createdAt" TEXT NOT NULL,
+                                         PRIMARY KEY("EIC"));
+
+CREATE TABLE IF NOT EXISTS "LineOrder" (
+                                         "OLIC" TEXT  NOT NULL,
+                                         "articleº" TEXT NOT NULL,
+                                         "load" INTEGER NOT NULL,
+                                         "unitPrice" REAL NOT NULL,
+                                         "totalPrice" REAL NOT NULL,
+                                         PRIMARY KEY("OLIC"));
+
+CREATE TABLE IF NOT EXISTS "LineReception" (
+                                          "RLIC" TEXT  NOT NULL,
+                                         "articleº" TEXT NOT NULL,
+                                         "load" INTEGER NOT NULL,
+                                         "unitPrice" REAL NOT NULL,
+                                         "totalPrice" REAL NOT NULL,
+                                         PRIMARY KEY("RLIC")
+                                           );
+
+CREATE TABLE IF NOT EXISTS "Order" (
+                                         "OIC" TEXT  NOT NULL,
+                                         "Customer" TEXT NOT NULL,   
+                                         "Price" REAL NOT NULL,
+                                         "Pay" String NOT NULL,
+                                         PRIMARY KEY("OIC"));
+
+CREATE TABLE IF NOT EXISTS "Reception" (
+                                         "RIC" TEXT  NOT NULL,
+                                         "Supplier" TEXT NOT NULL,   
+                                         "Carrier" TEXT NOT NULL,
+                                         "Cost" REAL NOT NULL,
+                                         PRIMARY KEY("RIC"));
+
+CREATE TABLE IF NOT EXISTS "Supplier" (
+                                         "SIC" TEXT  NOT NULL,
+                                         "nameSupplier" TEXT NOT NULL,   
+                                         "direction" TEXT NOT NULL,
+                                         "telephoneNumber" TEXT NOT NULL,
+					                    "email" TEXT NOT NULL,
+                                         PRIMARY KEY("SIC"));
+
+CREATE TABLE IF NOT EXISTS "User" (
+                                         "idUser" INTERGER  NOT NULL,
+                                         "username" TEXT NOT NULL,   
+                                         "password" TEXT NOT NULL,
+                                         PRIMARY KEY("idUser"),
+                                        FOREIGN KEY ("idUser") REFERENCES "Employee"("idUser") ON DELETE CASCADE
+);
---
+++ Programa/GestionAlmacenDAM/src/main/java/com/dam/gestionalmacendam/models/Employee.java
@@ -0,0 +1,32 @@
+package com.dam.gestionalmacendam.models;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class Employee extends User {
+    private final UUID EIC = UUID.randomUUID();
+    private String name;
+    private String surname;
+    private String nif;
+    private String email;
+    private String photo;
+    private boolean isManager;
+    private LocalDateTime createdAt;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "EIC=" + EIC +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", nif='" + nif + '\'' +
+                ", email='" + email + '\'' +
+                ", photo='" + photo + '\'' +
+                ", isManager=" + isManager +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
