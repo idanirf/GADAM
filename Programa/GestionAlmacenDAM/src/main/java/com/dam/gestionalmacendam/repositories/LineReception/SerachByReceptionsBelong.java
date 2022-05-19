83492c454d75c044726ce211ac3470953ac45338
Daniel Rodríguez Fernández <daniel.rodriguezfernandez@alumno.iesluisvives.org>
2022-05-19 20:54:53 +0200

    Act2.1 drodriguez 19/05

--- Programa/GestionAlmacenDAM/src/main/java/com/dam/gestionalmacendam/repositories/LineReception/SerachByReceptionsBelong.java
+++
@@ -1,9 +0,0 @@
-package com.dam.gestionalmacendam.repositories.LineReception;
-
-import javafx.collections.ObservableList;
-
-import java.sql.SQLException;
-
-interface SerachByReceptionsBelong<T,ID> {
-    ObservableList<T> SerachByReceptionsBelong(ID identifier) throws SQLException;
-}
