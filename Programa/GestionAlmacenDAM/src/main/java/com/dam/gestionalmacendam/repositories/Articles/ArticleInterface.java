d5ddf4b5541c91b17c4e6e2b23ad156fc8e921e0
Azahara <blancaluna92@hotmail.com>
2022-05-20 10:35:16 +0200

    preparar para merge

--- Programa/GestionAlmacenDAM/src/main/java/com/dam/gestionalmacendam/repositories/Articles/ArticleInterface.java
+++
@@ -1,9 +0,0 @@
-package com.dam.gestionalmacendam.repositories.Articles;
-
-import com.dam.gestionalmacendam.models.Article;
-import com.dam.gestionalmacendam.repositories.CRUDRepository;
-import com.dam.gestionalmacendam.repositories.SearchByUuid;
-
-
-public interface ArticleInterface extends CRUDRepository<Article, String>, SearchByName<Article, String>, SearchByUuid<Article, String> {
-}
