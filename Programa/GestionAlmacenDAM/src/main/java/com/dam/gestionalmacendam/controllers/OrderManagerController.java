a76f7bc24f4e54aad37785f056aab331b49c545d
Azahara <blancaluna92@hotmail.com>
2022-05-22 23:13:29 +0200

    compila y abre pantalla con datos

--- Programa/GestionAlmacenDAM/src/main/java/com/dam/gestionalmacendam/controllers/OrderManagerController.java
+++ Programa/GestionAlmacenDAM/src/main/java/com/dam/gestionalmacendam/controllers/OrderManagerController.java
@@ -19,42 +19,27 @@ public class OrderManagerController {
     //mi repositorio de order
     OrderRepository repository = OrderRepository.getInstance(DataBaseManager.getInstance());
 
-    //Logger logger = LogManager.getLogger(pedidoManagerViewController.class);
 
-    //para rellenar datos que no funciona
-    //@FXML
-    //private TableView<Order> tablaPedidos;
 
-    //@FXML
-    //private TableColumn<Order, String> columnaCliente;
-
-    //@FXML
-    //private TableColumn<?, ?> columnaMetodoDePago;
-
-    //@FXML
-    //private TableColumn<Order, String> columnaOIC;
-
-    //@FXML
-    //private TableColumn<Order, Double> columnaPrecio;
 
     @FXML
     private TextField textAreaBuscarPorOic;
 
 
     @FXML
-    private TableView<?> tablaPedidos;
+    private TableView<Order> tablaPedidos;
 
     @FXML
-    private TableColumn<?, ?> columnaCliente;
+    private TableColumn<Order, String> columnaCliente;
 
     @FXML
-    private TableColumn<?, ?> columnaMetodoDePago;
+    private TableColumn<Order, Pay> columnaMetodoDePago;
 
     @FXML
-    private TableColumn<?, ?> columnaOIC;
+    private TableColumn<Order, String> columnaOIC;
 
     @FXML
-    private TableColumn<?, ?> columnaPrecio;
+    private TableColumn<Order, Double> columnaPrecio;
 
 
 
@@ -83,39 +68,21 @@ public class OrderManagerController {
 
     }
 
-
-    @FXML
-    public void salirAction(MouseEvent mouseEvent) {
-
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Salir");
-        alert.setContentText("¿Está seguro que desea salir?");
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {
-            Platform.exit();
-        } else {
-            alert.close();
-        }
-
-
-    }
-
     @FXML
     private void initialize() {
-        // Cargo la lista de personas en base al observable
-        // try {
-        //     loadData();
-        //  } catch (SQLException e) {
-        //     System.out.println("No se ha podido cargar la lista de personas");
-        //}
 
-        // Asigno las columnas de la tabla
-        //columnaOIC.setCellValueFactory(cellData -> cellData.getValue().getOIC());
-        //columnaCliente.setCellValueFactory(cellData -> cellData.getValue().getCustomer());
-        //columnaMetodoDePago.setCellValueFactory(cellData -> cellData.getValue().);
-        //columnaPrecio.setCellValueFactory(cellData -> cellData.getValue().Integer.parseInt(getPrice()).);
+         try {
+             loadData();
+         } catch (SQLException e) {
+             System.out.println("No se ha podido cargar la lista de personas");
+         }
 
-        //tablaPedidos.getSelectionModel().selectFirst();
+        columnaOIC.setCellValueFactory(cellData -> cellData.getValue().getOIC());
+        columnaCliente.setCellValueFactory(cellData -> cellData.getValue().getCustomer());
+        columnaMetodoDePago.setCellValueFactory(cellData -> cellData.getValue().getMethodPay());
+        columnaPrecio.setCellValueFactory(cellData -> cellData.getValue().priceProperty().asObject());
+
+        tablaPedidos.getSelectionModel().selectFirst();
 
     }
     @FXML
@@ -128,7 +95,7 @@ public class OrderManagerController {
             System.out.println("Repositorio NO vacio");
             System.out.println(repository.findAll());
         }
-        //tablaPedidos.setItems(repository.findAll());
+        tablaPedidos.setItems(repository.findAll());
 
     }
 
